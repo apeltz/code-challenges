f603449e42a63eaf262a69709adc0fa2e00eec16
Aaron Peltz <aaron.peltz@gmail.com>
2016-08-22 19:27:24 -0700

    handles spoken numbers up to one billion

--- problem-17.js
+++ problem-17.js
@@ -4,22 +4,57 @@
     in words, how many letters would be used?
  */
 
+ function spokenNum(int) {
+   const suffixes = [null, 'thousand', 'million', 'billion']
+   let intArr = int.toString().split(/(?=(?:...)*$)/).map( e => Number(e))
+   let spoken = '';
+   for(let i=intArr.length-1, suf=0; i>=0; i--, suf++){
+     let suffix = suf ? suffixes[suf]:'';
+     spoken = spokenNumSub999(intArr[i]) + ' ' + suffix + ' ' + spoken;
+   }
+   return spoken
+ }
+
 // Limited at 1000
 function spokenNumSub999(int) {
+    if(int === 000) return ''
     const low = [
       'zero','one','two','three','four','five','six','seven','eight','nine',
       'ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','eighteen','nineteen',
     ]
     const tens = [null, null, 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+
     let intArr = int.toString().split('').map(d => Number(d))
-    if (intArr.length > 2 && intArr[intArr.length-2] === 1) {
+    if (intArr.length >= 2 && intArr[intArr.length-2] === 1) {
       intArr[intArr.length-1] = Number(intArr[intArr.length-2].toString()+intArr.pop())
       intArr[intArr.length-2] = null
     }
-    let spoken = ''
-    intArr.forEach(digit => )
-    console.log(intArr)
+    while(intArr.length < 3) intArr.unshift(null)
+    let spoken = '';
+    // Hundreds digit
+    if(intArr[0]) spoken += low[intArr[0]] + ' hundred'
+    // Tens digit, for values >= 20
+    if(intArr[0] && (intArr[1] || intArr[2])) spoken += ' and '
+    if(intArr[1]) spoken += tens[intArr[1]]
+    // Ones digit and teens
+    if(intArr[1] && intArr[2]) spoken += '-'
+    spoken += intArr[2] ? low[intArr[2]]:'';
+
+    return spoken
 }
 
+// Concats all spoken numbers under integer argument, inclusive
+function concatSpoken(int){
+  let spoken = ''
+  while(int) {
+    spoken += spokenNum(int)
+    int--
+  }
+  return spoken
+}
 
-spokenNumSub999(315)
+function countLetters(int){
+  return concatSpoken(int).match(/[A-Za-z]/g).length
+}
+
+console.log(countLetters(1000))
